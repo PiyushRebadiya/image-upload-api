0c471561e45cd0f8cfb98a0076da791c0d8a16c8
piyushrebadiya <piyushrebadiya123@gmail.com>
2022-06-18 21:38:28 +0530

    add some file

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose');
 const app = express();
 const port = 4500
 
-var route =  require('./route')
+var route = require('./route')
+
+mongoose.connect(
+  process.env.DB_ACCESS,
+  {useUnifiedTopology: true,useNewUrlParser:true},
+  () => console.log("connect to mongoDB!!!")
+)
+
+app.use(express.json())
 app.use(cors())
 app.use('/',route)
 
